117fb820ceb50b2f0af6ae76eff34de3ed673873
frost-head <ed.ayush2003@gmail.com>
2025-06-27 10:12:33 +0530

    bug fix

--- run.sh
+++ run.sh
@@ -4,14 +4,11 @@ SESSION_NAME="my_session"
 COMMAND="./main.sh"
 TMUX_CONF="./utils/tmux.conf"
 
-# Use custom tmux config
-export TMUX_CONF
-export TMUX="-f $TMUX_CONF"
-
-if tmux has-session -t "$SESSION_NAME" 2>/dev/null; then
-  tmux attach-session -t "$SESSION_NAME"
+if tmux -f "$TMUX_CONF" has-session -t "$SESSION_NAME" 2>/dev/null; then
+  tmux -f "$TMUX_CONF" attach-session -t "$SESSION_NAME"
 else
-  tmux new-session -d -s "$SESSION_NAME"
+  tmux -f "$TMUX_CONF" new-session -d -s "$SESSION_NAME"
+
 
   tmux split-window -h -t "$SESSION_NAME"
 
