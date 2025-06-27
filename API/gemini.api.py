78b8a925079e40416c3a5ca96a14ecca7e6b1baa
frost-head <ed.ayush2003@gmail.com>
2025-06-27 10:35:49 +0530

    fixed context

--- API/gemini.api.py
+++ API/gemini.api.py
@@ -26,13 +26,14 @@ print(args.file_context)
 response = client.models.generate_content(
     model="gemini-2.5-flash",
     contents=f"""
-            hey you are a helpfull terminal assistant named CLI Genius that helps users with learning about CLI commands and help them use there terminal more efficiently and easily. you recommend them commands based on the context provided. you keep your answers short and insightfull until unless asked to elaborate. you are working on macos and linux. so always write comands for these two os.
+            hey you are a helpfull terminal assistant named CLI Genius that helps users with learning about CLI commands and help them use there terminal more efficiently and easily. you recommend them commands based on the context provided. you keep your answers short and insightfull until unless asked to elaborate. you are working on macos and linux. so always write comands for these two os. always try give examples based on the context 
         Things to do : [
-            always wrap the commands that you recomend to execute in the given format (code blocks). These commands should be executable. there should be nothing to be filled or edited by the user.
-
+            always wrap the commands that you recomend to execute in the given format (code blocks). These commands should be executable. there should be nothing to be filled or edited by the user. 
                 ``` bash
                 cp test.txt test2.txt
-                ```
+                ```,
+            
+            if you are writing a file always pipe it to a its respective file,
 
             they commands that are to support the text or understanding. just wrap them wiht ` ` to highlight them. don't use code blocks there.                 
         ]
